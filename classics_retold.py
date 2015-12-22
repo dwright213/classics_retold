16bdbb87d21c95927e792642f92d8545ee9e9930
Dan Wright <muscle.crab.13@gmail.com>
2015-12-21 18:28:02 -0800

    delete erroneous requirements file

--- classics_retold.py
+++ classics_retold.py
@@ -1,6 +1,7 @@
 # standard libraries
 from IPython import embed
 from flask import Flask, jsonify
+from imgurpython import ImgurClient
 import os
 import json
 import pytumblr
@@ -10,10 +11,19 @@ import re
 import random
 import imghdr
 import requests
-from imgurpython import ImgurClient
 
+# sqlite database connection
+def sqlite_connect(toggle):
+    conn = sqlite3.connect('moby_sentences.practice_delete.sql')
+    c = conn.cursor()
+    if toggle == True:
+        return c
+    elif toggle == False:
+        conn.commit()
+        conn.close()
 
-
+def vacuum_sqlite():
+    c.execute("VACUUM")
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.cfg')
@@ -44,22 +54,30 @@ def get_images(search_string):
     return jsonify(results_dict)
 
 @app.route('/sqlite/count')
-def search_database():
-    conn = sqlite3.connect('moby_sentences.sql')
-    c = conn.cursor()
+def count_sentences():
+    c = sqlite_connect(True)
     count_entries = "SELECT count(sent_text) AS count FROM sentences_grabbed"
     sentence_number = c.execute(count_entries).fetchmany()
-    conn.close()
+    sqlite_connect(False)
     return str(sentence_number)
 
 @app.route('/sqlite/get/<record_number>')
 def get_sentence(record_number):
-    conn = sqlite3.connect('moby_sentences.sql')
-    c = conn.cursor()
+    c = sqlite_connect(True)
     get_sentence = "SELECT * FROM sentences_grabbed WHERE ROWID =?"
     sentence_got = c.execute(get_sentence,(record_number,)).fetchone()
-    conn.close()
+    sqlite_connect(False)
     return str(sentence_got)
+
+@app.route('/sqlite/delete/<record_number>')
+def delete_sentence(record_number):
+    conn = sqlite3.connect('moby_sentences.practice_delete.sql')
+    c = conn.cursor()
+
+    delete_sentence = "DELETE FROM sentences_grabbed WHERE ROWID =?"
+    c.execute(delete_sentence,(record_number,))
+    sqlite_connect(False)
+    return str('sentence number '+ record_number +' deleted! ')
 
 
 if __name__ == "__main__":
